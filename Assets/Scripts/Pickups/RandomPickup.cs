133ab661a04ec5aa92ded203abb6bbc8ff78ece7
WizyWoo <kevin.hagfors@gmail.com>
2022-01-21 11:47:40 +0100

    polp

---
+++ Assets/Scripts/Pickups/RandomPickup.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPickup : MonoBehaviour , IPickup
+{
+
+    public void Interact()
+    {
+
+        Debug.Log("ass");
+
+    }
+
+}
