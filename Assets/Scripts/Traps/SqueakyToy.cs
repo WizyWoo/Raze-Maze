781e3cc67744ad64a9bc4a3d54945f89e8299927
heimir-s <heimirsindri@icloud.com>
2022-02-08 13:02:42 +0100

    beastscript update

--- Assets/Scripts/Traps/SqueakyToy.cs
+++ Assets/Scripts/Traps/SqueakyToy.cs
@@ -1,23 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class SqueakyToy : MonoBehaviour
 {
     public GameObject beast;
     Vector3 spawnPlacement;
     public Vector3 fwd;
+    public Text beastMessage;
+    float textTimer = 3;
+    public float beastTimer = 5;
+    public bool trapTriggered = false;
+    Renderer rend;
+    GameObject victim;
 
     private void Start()
     {
         fwd.Set(0, -0.5f, 4);
-        spawnPlacement = transform.position + fwd - (Vector3.down) * 0;
-        
+        rend = gameObject.GetComponentInChildren<Renderer>();
+        beastMessage.enabled = false;
+    }
+
+    private void Update()
+    {
+        if (beastTimer > 0 && trapTriggered == true)
+        {
+            beastTimer -= Time.deltaTime;
+        }
+        else if (beastTimer == 0)
+        {
+            SpawnBeast();   
+        }
+
+        if (textTimer > 0)
+        {
+            textTimer -= Time.deltaTime;
+        }
+        else beastMessage.enabled = false;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        beast = Instantiate(beast, spawnPlacement, transform.rotation * Quaternion.Euler(0, 180, 0));
-        Destroy(gameObject); 
+        textTimer = 3;
+        beastTimer = 5;
+        trapTriggered = true;
+        victim = other.gameObject;
+        beastMessage.enabled = true;        
+        rend.enabled = false;
+        gameObject.GetComponent<Collider>().enabled = false;
+    }
+
+    void SpawnBeast()
+    {
+        for (int i = 0; i <= 5; i++)
+        {
+            spawnPlacement = victim.transform.position + fwd;
+            beast = Instantiate(beast, spawnPlacement, transform.rotation * Quaternion.Euler(0, 180, 0));
+        }
     }
 }
