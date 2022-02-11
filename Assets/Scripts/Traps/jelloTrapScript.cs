58a1350f66e057e479d075f1caf68f974bbace6b
Quick-Bolt <nuno.gnu1@gmail.com>
2022-02-11 23:26:30 +0100

    Update jelloTrapScript.cs

--- Assets/Scripts/Traps/jelloTrapScript.cs
+++ Assets/Scripts/Traps/jelloTrapScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class jelloTrapScript : TrapController
+public class JelloTrapScript : TrapController
 {
     private float jelloMultiplier = 1f;
 
