e873dce01bd31a5caab12eba96086a9ed026f586
WizyWoo <kevin.hagfors@gmail.com>
2022-02-23 18:50:44 +0100

    iu8gyh

---
+++ Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    //This script controls the players camera and their Field of view when running.
+
+    public bool Sprinting;
+    public float InputSensitivity, FovChangeSpeed;
+    [SerializeField]
+    private Transform cameraPivot;
+    private float xRot, fov, sprint;
+    private Transform player;
+    private PlayerMovementController pMC;
+    private Rigidbody rB;
+
+    void Start()
+    {
+
+        fov = Camera.main.fieldOfView;
+        Cursor.lockState = CursorLockMode.Locked;
+        player = transform.root;
+        pMC = player.GetComponent<PlayerMovementController>();
+        rB = player.GetComponent<Rigidbody>();
+
+    }
+
+    void Update()
+    {
+
+        player.Rotate(Vector3.up, Input.GetAxisRaw("Mouse X") * InputSensitivity);
+        xRot -= Input.GetAxisRaw("Mouse Y") * InputSensitivity;
+        xRot = Mathf.Clamp(xRot, -90, 90);
+        cameraPivot.localEulerAngles = new Vector3(xRot, 0, 0);
+
+        if(Sprinting && sprint != 1)
+        {
+
+            sprint = Mathf.Clamp(sprint + (Time.deltaTime * FovChangeSpeed), 0, 1);
+            Camera.main.fieldOfView = fov + (10 * sprint);
+
+        }
+        else if(!Sprinting && rB.velocity.magnitude < pMC.MovementSpeed * 1.2f)
+        {
+            
+            sprint = Mathf.Clamp(sprint - (Time.deltaTime * FovChangeSpeed), 0, 1);
+            Camera.main.fieldOfView = fov + (10 * sprint);
+
+        }
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+
+            if(Cursor.lockState == CursorLockMode.Locked)
+                Cursor.lockState = CursorLockMode.None;
+            else
+                Cursor.lockState = CursorLockMode.Locked;
+
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -0,0 +1,144 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+
+    public float MovementSpeed, SprintSpeedModifier, JumpHeight, AirControlMult, CoyoteTime;
+    public bool DrawGizmos;
+    private float sprinting, coyoteTimer;
+    [SerializeField]
+    private bool grounded;
+    private Vector3 movementDir, spawnOrigin;
+    private Rigidbody rb;
+    private LayerMask playerMask;
+    private CameraController cc;
+    [SerializeField]
+    private Vector3 groundCheckV3, groundCheckDirV3;
+    [SerializeField]
+    private float groundCheckDist;
+
+    void Start()
+    {
+
+        cc = Camera.main.GetComponent<CameraController>();
+        rb = gameObject.GetComponent<Rigidbody>();
+        playerMask = 1 << LayerMask.NameToLayer("Player");
+        spawnOrigin = transform.position;
+
+    }
+
+    void Update()
+    {
+
+        if(transform.position.y < -5)
+        {
+
+            rb.velocity = Vector3.zero;
+            transform.position = spawnOrigin;
+
+        }
+
+        if(Physics.SphereCast(transform.position + groundCheckV3, groundCheckDist, groundCheckDirV3, out RaycastHit groundCheck, 1f, ~playerMask, QueryTriggerInteraction.Ignore))
+        {
+
+            grounded = true;
+            coyoteTimer = CoyoteTime;
+
+        }
+        else if(coyoteTimer <= 0)
+        {
+
+            grounded = false;
+
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && grounded == true)
+        {
+
+            rb.velocity = new Vector3(rb.velocity.x, JumpHeight, rb.velocity.z);
+            coyoteTimer = 0;
+
+        }
+
+        if(Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W))
+        {
+
+            sprinting = SprintSpeedModifier;
+            cc.Sprinting = true;
+
+        }
+        else
+        {
+
+            sprinting = 1;
+            cc.Sprinting = false;
+
+        }
+        
+        movementDir = (transform.forward * sprinting * Input.GetAxis("Vertical") * MovementSpeed) + (transform.right * sprinting * Input.GetAxis("Horizontal") * MovementSpeed);
+
+        if(grounded)
+        {
+
+            movementDir += new Vector3(0, rb.velocity.y, 0);
+
+        }
+        else
+        {
+
+            Vector3 airControlV3 = new Vector3();
+
+            if(Mathf.Abs(rb.velocity.x) < MovementSpeed * sprinting)
+            {
+
+                airControlV3.x = (movementDir.x * AirControlMult) * Time.deltaTime + rb.velocity.x;
+
+            }
+            else
+            {
+
+                airControlV3.x = rb.velocity.x;
+
+            }
+
+            if(Mathf.Abs(rb.velocity.z) < MovementSpeed * sprinting)
+            {
+
+                airControlV3.z = (movementDir.z * AirControlMult) * Time.deltaTime + rb.velocity.z;
+
+            }
+            else
+            {
+
+                airControlV3.z = rb.velocity.z;
+
+            }
+
+            airControlV3.y = rb.velocity.y;
+            movementDir = airControlV3;
+
+        }
+
+        rb.velocity = movementDir;
+
+        if(coyoteTimer > 0)
+            coyoteTimer -= Time.deltaTime;
+
+    }
+
+    private void OnDrawGizmos()
+    {
+
+        if(DrawGizmos)
+        {
+
+            Gizmos.color = Color.green;
+            Gizmos.DrawSphere(transform.position + groundCheckV3, groundCheckDist);
+
+        }
+
+    }
+
+}
