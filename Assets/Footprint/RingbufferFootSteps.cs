4d3baa9b1b583121368cadc6ff1eaf33aea83283
TheSkyShaft <Theskyshaft@hotmail.com>
2022-03-04 13:41:27 +0100

    Footprints test

---
+++ Assets/Footprint/RingbufferFootSteps.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class RingbufferFootSteps : MonoBehaviour
+{
+    public ParticleSystem system;
+    public Material activeMat;
+
+    Vector3 lastEmit;
+
+    public float delta = 1;
+    public float gap = 0.5f;
+    int dir = 1;
+    static RingbufferFootSteps selectedSystem;
+
+    void Start()
+    {
+        lastEmit = transform.position;
+        GetComponent<MeshRenderer>().material = activeMat;
+    }
+
+    public void Update()
+    {
+
+        if (Vector3.Distance(lastEmit, transform.position) > delta)
+        {
+            Gizmos.color = Color.green;
+            var pos = transform.position + (transform.right * gap * dir);
+            dir *= -1;
+            ParticleSystem.EmitParams ep = new ParticleSystem.EmitParams();
+            ep.position = pos;
+            ep.rotation = transform.rotation.eulerAngles.y;
+            system.Emit(ep, 1);
+            lastEmit = transform.position;
+        }
+
+    }
+}
