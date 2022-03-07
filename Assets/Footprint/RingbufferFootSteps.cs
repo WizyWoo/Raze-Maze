2deffc8e599dd55a966815defb237e3521c838e7
TheSkyShaft <TheSkyShaft@hotmail.com>
2022-03-07 10:58:23 +0100

    Added ground check to footprintaligner and made sure footprints only appear on the ground

--- Assets/Footprint/RingbufferFootSteps.cs
+++ Assets/Footprint/RingbufferFootSteps.cs
@@ -12,26 +12,27 @@ public class RingbufferFootSteps : MonoBehaviour
     public float gap = 0.5f;
     int dir = 1;
     static RingbufferFootSteps selectedSystem;
+    private FootprintAligner _fa;
 
     void Start()
     {
         lastEmit = transform.position;
-        GetComponent<MeshRenderer>().material = activeMat;
+        _fa = GetComponent<FootprintAligner>();
     }
 
     public void Update()
     {
-
-        if (Vector3.Distance(lastEmit, transform.position) > delta)
-        {
-            Gizmos.color = Color.green;
-            var pos = transform.position + (transform.right * gap * dir);
-            dir *= -1;
-            ParticleSystem.EmitParams ep = new ParticleSystem.EmitParams();
-            ep.position = pos;
-            ep.rotation = transform.rotation.eulerAngles.y;
-            system.Emit(ep, 1);
-            lastEmit = transform.position;
+        if(_fa.grounded || _fa == null){
+            if (Vector3.Distance(lastEmit, transform.position) > delta){
+                Gizmos.color = Color.green;
+                var pos = transform.position + (transform.right * gap * dir);
+                dir *= -1;
+                ParticleSystem.EmitParams ep = new ParticleSystem.EmitParams();
+                ep.position = pos;
+                ep.rotation = transform.rotation.eulerAngles.y;
+                system.Emit(ep, 1);
+                lastEmit = transform.position;
+            }
         }
 
     }
