9ab39f7fbd32ce48883325d14979b773bdbf1871
Quick-Bolt <nuno.gnu1@gmail.com>
2022-03-08 09:41:42 +0100

    weeping angel :angel:

---
+++ Assets/WeepingAngel.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeepingAngel : TrapController
+{
+    public GameObject player;
+    private new Camera camera;
+    private new Renderer renderer;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        camera = Camera.main;
+        renderer = GetComponent<Renderer>();
+    }
+
+    private void Update()
+    {
+        bool isVisible = GeometryUtility.TestPlanesAABB(
+            GeometryUtility.CalculateFrustumPlanes(camera),
+            renderer.bounds);
+
+        if (!isVisible)
+            TryMovingTowardsPlayer();
+    }
+
+    private void TryMovingTowardsPlayer()
+    {
+        if (player == null)
+            return;
+
+        transform.position = player.transform.position - player.transform.forward;
+        Vector3 lookPos = player.transform.position - transform.position;
+        lookPos.y = 0;
+        transform.rotation = Quaternion.LookRotation(lookPos);
+    }
+}
