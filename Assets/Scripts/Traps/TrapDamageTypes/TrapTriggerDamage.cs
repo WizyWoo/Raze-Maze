61eed6c0cc928a3b82caa4508041aa2b9d5426db
WizyWoo <kevin.hagfors@gmail.com>
2022-03-13 19:21:54 +0100

    ihuiug

---
+++ Assets/Scripts/Traps/TrapDamageTypes/TrapTriggerDamage.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Com.MyCompany.MyGame;
+
+public class TrapTriggerDamage : TrapController
+{
+
+    public bool ContinueslyDamage;
+
+    public override void ActivateTrap()
+    {
+
+        base.ActivateTrap();
+
+        gameObject.GetComponent<Collider>().isTrigger = true;
+        
+    }
+
+    private void FixedUpdate()
+    {
+
+        if(TrapActived)
+            gameObject.GetComponent<Collider>().isTrigger = true;
+
+    }
+    
+    private void OnTriggerEnter(Collider _other)
+    {
+
+        if(!ContinueslyDamage)
+        {
+
+            if(_other.transform.root.TryGetComponent<PlayerManager>(out PlayerManager _tempPM))
+            {
+
+                _tempPM.Damage(Damage);
+
+            }
+
+        }
+
+    }
+
+    private void OnTriggerStay(Collider _other)
+    {
+
+        if(ContinueslyDamage)
+        {
+
+            if(_other.transform.root.TryGetComponent<PlayerManager>(out PlayerManager _tempPM))
+            {
+
+                _tempPM.Damage(Damage);
+
+            }
+
+        }
+
+    }
+
+}
