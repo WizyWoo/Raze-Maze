9b1dcdef352992dcffb0eee1f968865f4be787c4
heimir-s <heimirsindri@icloud.com>
2022-04-06 15:19:49 +0200

    Update FootstepsSound.cs

--- Assets/Scripts/Audio/FootstepsSound.cs
+++ Assets/Scripts/Audio/FootstepsSound.cs
@@ -1,23 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FootstepsSound : MonoBehaviour
 {
-    public PlayerMovementController Ref;
-
     // Update is called once per frame
     void Update()
     {
-        if(Ref.grounded == true && Input.GetKeyDown(KeyCode.W))
+        if(Input.GetKeyDown(KeyCode.W))
         {
             PlayFootsteps();
         }
-        if(Input.GetKeyUp(KeyCode.W) || Ref.grounded == false)
+        if(Input.GetKeyUp(KeyCode.W))
         {
             PlayOff();
         }
-
     }
         
     void PlayFootsteps()
