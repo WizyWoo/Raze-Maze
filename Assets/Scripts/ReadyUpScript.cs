757db6d8a1fe39fc1ca8a7a29782a0f641a11209
Quick-Bolt <nuno.gnu1@gmail.com>
2022-04-08 10:19:48 +0200

    erm.. apparently i count count?!

--- Assets/Scripts/ReadyUpScript.cs
+++ Assets/Scripts/ReadyUpScript.cs
@@ -13,7 +13,7 @@ namespace Com.MyCompany.MyGame
 
         public GameObject chunkGenerator;
         private bool locked;
-        private int readyCounter = 4;
+        private int readyCounter = 3;
 
         public TextMesh readyText;
 
