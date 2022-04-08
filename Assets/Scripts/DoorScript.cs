bac66d0e91968092eaed5749f4e53f29162cf84f
Quick-Bolt <nuno.gnu1@gmail.com>
2022-04-08 11:51:57 +0200

    poopy

--- Assets/Scripts/DoorScript.cs
+++ Assets/Scripts/DoorScript.cs
@@ -4,48 +4,15 @@ using Photon.Pun;
 
 public class DoorScript : MonoBehaviourPunCallbacks, IInteractable
 {
-    public static bool doorKey = false;
-    public bool keycheck;
-    public bool open, close = true, inTrigger;
+    public bool open;
     private Animation anim;
 
-    private void Start()
+    void OnTriggerEnter(Collider other)
     {
-        anim = gameObject.GetComponent<Animation>();
-        keycheck = doorKey;
-    }
-
-    void Update()
-    {
-        keycheck = doorKey;
-    }
-
-    public void Activate(Transform _player)
-    {
-            if (close)
-            {
-                if (doorKey)
-                {
-                    open = true;                   
-                    close = false;
-                }
-            }
-            else
-            {
-                close = true;
-                open = false;                
-            }
-
-            if (open)
-            {
-              photonView.RPC("OpenDoor", RpcTarget.All);
-            }                    
-    }
-
-    [PunRPC]
-    private void OpenDoor()
-    {
-        if (anim != null)
-             anim.Play();
+        if(other.TryGetComponent<KeyScript>(out KeyScript key))
+        {
+            if (anim != null)
+                anim.Play();
+        }
     }
 }
--- Assets/Scripts/KeyScript.cs
+++ Assets/Scripts/KeyScript.cs
@@ -3,24 +3,5 @@ using System.Collections;
 
 public class KeyScript : MonoBehaviour, IInteractable
 {
-    private bool inTrigger;
-
-    void OnTriggerEnter(Collider other)
-    {
-        inTrigger = true;
-    }
-
-    void OnTriggerExit(Collider other)
-    {
-        inTrigger = false;
-    }
-
-    public void Activate(Transform _player)
-    {
-        if (inTrigger)
-        {
-            DoorScript.doorKey = true;
-            Destroy(this.gameObject);
-        }
-    }
+    
 }
