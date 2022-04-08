4338e4d689211fe923a95bbda345a26b8e8de1d1
Quick-Bolt <nuno.gnu1@gmail.com>
2022-04-08 15:26:32 +0200

    poopy

--- Assets/Scripts/DoorScript.cs
+++ Assets/Scripts/DoorScript.cs
@@ -7,9 +7,9 @@ public class DoorScript : MonoBehaviourPunCallbacks, IInteractable
     public bool open;
     private Animation anim;
 
-    void OnTriggerEnter(Collider other)
+    void OnCollisionEnter(Collision other)
     {
-        if(other.TryGetComponent<KeyScript>(out KeyScript key))
+        if(other.gameObject.TryGetComponent<KeyScript>(out KeyScript key))
         {
             if (anim != null)
                 anim.Play();
