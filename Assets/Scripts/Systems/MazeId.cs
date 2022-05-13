c1476e63d4e8f871e132ce7683d3a4d009542f19
TheSkyShaft <Theskyshaft@hotmail.com>
2022-05-13 12:13:40 +0200

    Update MazeId.cs

--- Assets/Scripts/Systems/MazeId.cs
+++ Assets/Scripts/Systems/MazeId.cs
@@ -6,4 +6,32 @@ public class MazeId : MonoBehaviour
 {
     public List<MazeDoorIndicator> wallDoors;
     public Vector3 placeLocation;
+    public GameObject archway;
+
+    void Start(){
+        // add archway to every wall door
+        if(archway != null){
+
+            foreach (var item in wallDoors)
+            {
+                GameObject arch = Instantiate(archway, item.transform.position, Quaternion.identity);
+                arch.transform.parent = transform.GetChild(0).transform;
+                switch (item.gameObject.name.ToString())
+                {
+                    case "R":
+                    break;
+                    case "L":
+                    break;
+                    case "B":
+                        Debug.Log("B");
+                        arch.transform.Rotate(new Vector3(0,90,0));
+                    break;
+                    case "T":
+                        Debug.Log("T");
+                        arch.transform.Rotate(new Vector3(0,90,0));
+                    break;
+                }
+            }
+        }
+    }
 }
