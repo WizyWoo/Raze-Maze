386e2a6ed73cbd51b530794441d45d357380dd13
Quick-Bolt <nuno.gnu1@gmail.com>
2022-05-13 13:27:33 +0200

    Update CheckPointScript.cs

--- Assets/Scripts/CheckPointScript.cs
+++ Assets/Scripts/CheckPointScript.cs
@@ -7,7 +7,7 @@ public class CheckPointScript : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.transform.root.CompareTag("Player"))
             GameManager.gameManager.lastCheckpointPos = other.transform.position;
     }
 }
