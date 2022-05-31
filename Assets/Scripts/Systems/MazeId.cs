aff49140d841892c5d0dc01f4346944c1b6306a3
TheSkyShaft <TheSkyShaft@hotmail.com>
2022-05-31 14:07:09 +0200

    Fixed archways not having own texture

--- Assets/Scripts/Systems/MazeId.cs
+++ Assets/Scripts/Systems/MazeId.cs
@@ -7,15 +7,17 @@ public class MazeId : MonoBehaviour
     public List<MazeDoorIndicator> wallDoors;
     public Vector3 placeLocation;
     public GameObject archway;
+    public List<GameObject> _archways = new List<GameObject>();
 
-    void Start(){
+    void Awake(){
         // add archway to every wall door
         if(archway != null){
 
             foreach (var item in wallDoors)
             {
                 GameObject arch = Instantiate(archway, item.transform.position, Quaternion.identity);
-                arch.transform.parent = transform.GetChild(0).transform;
+                _archways.Add(arch);
+                arch.transform.parent = transform;
                 switch (item.gameObject.name.ToString())
                 {
                     case "R":
@@ -23,13 +25,26 @@ public class MazeId : MonoBehaviour
                     case "L":
                     break;
                     case "B":
-                        Debug.Log("B");
                         arch.transform.Rotate(new Vector3(0,90,0));
                     break;
                     case "T":
-                        Debug.Log("T");
                         arch.transform.Rotate(new Vector3(0,90,0));
                     break;
+                }
+            }
+        }
+    }
+    public void CheckArchways(){
+        Debug.Log(_archways.Count);
+        foreach (var item in wallDoors)
+        {
+            if(item.gameObject.activeInHierarchy){
+                foreach (var arch in _archways)
+                {
+                    if(item.transform.position == arch.transform.position){
+                        Destroy(arch.gameObject);
+                        arch.gameObject.SetActive(false);
+                    }
                 }
             }
         }
