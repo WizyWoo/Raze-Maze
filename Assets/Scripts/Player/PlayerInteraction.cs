2e80f80d06d75540745c2df797664a8fc89cf7a1
WizyWoo <kevin.hagfors@gmail.com>
2022-06-09 16:47:35 +0200

    fixed it

--- Assets/Scripts/Player/PlayerInteraction.cs
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -16,7 +16,7 @@ public class PlayerInteraction : VRInputManager
     private void Start()
     {
 
-        interactMask = ((1 << LayerMask.NameToLayer("Interactables") + 1 << LayerMask.NameToLayer("Default")));
+        interactMask = ((1 << LayerMask.NameToLayer("Interactables")) + (1 << LayerMask.NameToLayer("Default")));
 
         if(DesktopMode)
             UpdateUIRefs();
